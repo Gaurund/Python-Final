53e8fcd48b0b0f43969574aba1bf105ba221a5db
Gaurund <pavel.makushev@gmail.com>
2024-04-04 20:59:48 +0300

    WIP Templates

--- cookbook/cookbookapp/urls.py
+++ cookbook/cookbookapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_recipe/', views.add_recipe_form, name='add_recipe_form'),
     path('login/', views.login_view, name='login_view'),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
